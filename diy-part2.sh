b953f2035305463a2a7e1210cf88d61f5f50e43f
netswimer <gyf319@gmail.com>
2025-11-16 11:42:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 sed -i 's/192.168.1.1/192.168.188.1/g' package/base-files/luci2/bin/config_generate
 #sed -i 's/LEDE/OpenWrt(2025.3.21)/g' package/base-files/luci2/bin/config_generate
-sed -i 's/LEDE/OpenWrt-'"$(date -d "now" +%Y-%m-%d)"'/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/EdgeRouter-'"$(date -d "now" +%Y-%m-%d)"'/g' package/base-files/luci2/bin/config_generate
